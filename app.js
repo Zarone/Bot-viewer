69b174ae8b5827a6bba545a59bd4dcec6ae76421
Lasse Schier Christiansen <lasse.10@hotmail.dk>
2018-03-06 19:48:39 +0100

    Inventory and prices tab functionallity are running

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var handlebars = require('express-handlebars');
 var logger = require('morgan');
 var TF2Items = require('tf2-items');
+var fs = require('graceful-fs');
 
 var Item = require('./app/item.js')
 
@@ -31,7 +32,87 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.render('home');
+    var inventoryPath = "./temp/inventory.json";
+    var pricesPath = "./temp/prices.json";
+    var limitPath = "./temp/limits.json";
+    var tableContent = [];
+    fs.readFile(pricesPath, 'utf8', function(err, contents){
+        var data = JSON.parse(contents);
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+
+            function trunc(number, decimals = 2) {
+                const factor = Math.pow(10, decimals);
+                return Math.floor(number * factor) / factor;
+            };
+
+            function plural(word, count) {
+                return count == 1 ? word : word + 's';
+            };
+
+            function decimalPlaces(num) {
+                var match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+                if (!match) { return 0; }
+                return Math.max(
+                    0,
+                    // Number of digits right of decimal point.
+                    (match[1] ? match[1].length : 0)
+                    // Adjust for scientific notation.
+                    - (match[2] ? +match[2] : 0));
+            }
+
+            function currencyAsText(currencies) {
+                var text = '';
+
+                if (currencies.keys && currencies.keys != 0) {
+                    text = currencies.keys + ' ' + plural('key', currencies.keys);
+                }
+                if (currencies.metal && currencies.metal != 0) {
+                    if (text != '') {
+                        text += ', ';
+                    }
+                    text += (decimalPlaces(currencies.metal) == 2 ? currencies.metal : trunc(currencies.metal, 2)) + ' ref';
+                }
+                if (text == '') {
+                    return '0 keys, 0 ref';
+                }
+
+                return text;
+            };
+
+            buyPrice = currencyAsText(element.price.buy);
+            sellPrice = currencyAsText(element.price.sell);
+
+            tableContent.push({
+                "name": element.item.name,
+                "buyPrice": buyPrice,
+                "sellPrice": sellPrice,
+                "amount": 0,
+            });
+        };
+        fs.readFile(inventoryPath, 'utf8', function(err, contents){
+            var data = JSON.parse(contents);
+            for (let i = 0; i < data.length; i++) {
+                const element = data[i].name;
+                for (let j = 0; j < tableContent.length; j++) {
+                    if (tableContent[j].name == element) {
+                        tableContent[j].amount++;
+                    };
+                };
+            };
+            fs.readFile(limitPath, 'utf8', function(err, contents){
+                var data = JSON.parse(contents);
+                for (let i = 0; i < tableContent.length; i++) {
+                    if (data[tableContent[i].name] != -1) {
+                        tableContent[i].amount = tableContent[i].amount + '/' + data[tableContent[i].name];
+                    };
+                };
+                res.render('home', {data: JSON.stringify(tableContent)});
+                //console.log(tableContent);
+            });
+        });
+    });
+    
 });
 
 app.use(function (err, req, res, next) {
